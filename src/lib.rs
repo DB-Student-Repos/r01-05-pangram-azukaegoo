600ccc23b130b90fabd3ffdd12a93ab29854ae5c
Gloria Azum <azumgloria@gmail.com>
2024-03-17 23:13:39 +0100

    updated from for to all

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,5 @@ pub fn is_pangram(sentence: &str) -> bool {
 
     let sentence = sentence.to_lowercase();
 
-    for iterate in 'a'..='z' {
-        match sentence.contains(iterate) {
-            false => return false,
-            true => continue,
-        }
-    }
-    true
+    ('a'..='z').all(|letter| sentence.contains(letter))
 }
